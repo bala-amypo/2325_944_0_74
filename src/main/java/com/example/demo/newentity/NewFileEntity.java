3f7954b64e8f697cc313fe818dc60cd5f5d40bfd
bala-amypo <bala@amypo.in>
2025-12-17 08:25:00 +0000

    User commit

--- src/main/java/com/example/demo/newentity/NewFileEntity.java
+++ src/main/java/com/example/demo/newentity/NewFileEntity.java
@@ -1,24 +1,61 @@
 package com.example.demo.newentity;
-import jakarta.util.persistence.*;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Email;
-
-public class NewFileEntity{
+@Entity
+@Table(name = "Student")
+public class NewfileEntity{
     @Id
-    @GeneratedValue(strategy=GeneratedType.IDENTITY)
+    @GenerateValue(strategy=GenerateType.IDENTITY)
     private Long id;
-    @NotBlank(message="Should not contain spaces")
+    @NotBlank(message = "Should not contain spaces")
     private String name;
-    @Column(name=unique)
-    @NotBlank(message="Should not contain spaces")
-    @Email(message="Invalid message")
+    @NotBlank(message = "Email dosent not contain White Spaces")
+    @Email(message = "Invalid format")
     private String email;
 
-    public NewFileEntity(){
+    public NewfileEntity(Long id, @NotBlank(message = "Should not contain spaces")String name,@NotBlank(message = "Email dosent not contain White Spaces")@Email(message = "Invalid format") String email){
         this.id = id;
         this.name = name;
         this.email = email;
     }
 
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
     
+
+}package com.example.demo.newentity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+@Entity
+@Table(name = "Student")
+public class NewfileEntity{
+    @Id
+    @GenerateValue(strategy=GenerateType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Should not contain spaces")
+    private String name;
+    @NotBlank(message = "Email dosent not contain White Spaces")
+    @Email(message = "Invalid format")
+    private String email;
+
+    public NewfileEntity(Long id, @NotBlank(message = "Should not contain spaces")String name,@NotBlank(message = "Email dosent not contain White Spaces")@Email(message = "Invalid format") String email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    
+
 }
